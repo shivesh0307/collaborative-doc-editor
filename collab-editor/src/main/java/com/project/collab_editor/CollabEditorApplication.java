f41073ab37a20ad672c1916d74fc3276ef80c4db
Shivesh Dixit <dixitshivesh55@gmail.com>
2025-09-22 19:44:14 +0530

    Initialize collab-editor project with Spring Boot, Docker, and Redis configuration

---
+++ collab-editor/src/main/java/com/project/collab_editor/CollabEditorApplication.java
@@ -0,0 +1,13 @@
+package com.project.collab_editor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollabEditorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollabEditorApplication.class, args);
+	}
+
+}
---
+++ collab-editor/src/test/java/com/project/collab_editor/CollabEditorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.collab_editor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollabEditorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
